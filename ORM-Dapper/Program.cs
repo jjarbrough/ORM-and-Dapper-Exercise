d2fb4781e2807b1a30b3cb70f12f8046d343843a
James Yarbrough <jjarbrough@gmail.com>
2023-10-30 13:10:08 -0500

    finished exercise two

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -17,6 +17,8 @@ namespace ORM_Dapper
 
             IDbConnection conn = new MySqlConnection(connString);
 
+            //testing the departments get and add
+
             var departmentRepo = new DapperDepartmentRepository(conn);
             departmentRepo.InsertDepartment("Office Supplies");
             var departments = departmentRepo.GetAllDepartments();
@@ -24,6 +26,17 @@ namespace ORM_Dapper
             {
                 Console.WriteLine($"{department.DepartmentID} | {department.Name}");
             }
+
+            //testing the products methods
+
+            var productRepo = new DapperProductRepository(conn);
+            productRepo.CreateProduct("Pen", 1.99, 5);
+            var products = productRepo.GetAllProducts();
+            foreach(var product in products)
+            {
+                Console.WriteLine($"{product.Name} | {product.Price} | {product.CategoryID}");
+            }
+
         }
     }
 }
